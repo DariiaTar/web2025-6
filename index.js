00f1fa9b71e200fe98a8bc85f5c121a553b6c423
Dariia Taradaika bc2025-2 <dariia.taradaika@gmail.com>
2025-05-06 15:45:51 +0300

    Implemented POST /write with redirect to form after submission

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const http = require("http")
 const { Command } = require ("commander");
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const multer = require('multer'); 
+const upload = multer();  
 
 const program = new Command();
 
@@ -11,11 +16,54 @@ program
 program.parse(process.argv);
 const options = program.opts();
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Serveris running\n");
-}) ;
+const app = express();
+app.use(express.text()); // для PUT
+app.use(express.json()); // для JSON
 
-server.listen(options.port, options.host, () => {
+let notes = {};
+const cachePath = options.cache;
+
+function loadNotes() {
+    if (fs.existsSync(cachePath)) {
+    const data = fs.readFileSync(cachePath, 'utf-8');
+    notes = JSON.parse(data);
+    }
+}
+
+function saveNotes() {
+    fs.writeFileSync(cachePath, JSON.stringify(notes, null, 2));
+}
+
+loadNotes();
+
+
+
+app.get('/UploadForm.html', (req, res) => {
+    const filePath = path.join(__dirname, 'UploadForm.html');
+    if (!fs.existsSync(filePath)) return res.sendStatus(404);
+    
+    res.sendFile(filePath);
+});
+
+
+app.post("/write", upload.none(), (req, res) => {
+    const name = req.body.note_name?.trim();
+    const text = req.body.note?.trim();
+    if (!name || !text) return res.status(400).send("Missing note_name or note");
+
+    if (notes[name]) {
+     return res.status(400).send("Note already exists");
+    }
+
+    notes[name] = text;
+    saveNotes();
+    // res.sendStatus(201);
+    res.redirect('/UploadForm.html')
+  });
+
+
+
+app.listen(options.port, options.host, () => {
     console.log(`Server running at http://${options.host}:${options.port}/`);
 })
+
